4cd829b38f031a4adaa0da0dd7011812b71b7015
MAHossain1 <mahpro110@gmail.com>
2023-06-01 19:52:12 +0600

    [DONE] create routes and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import usersServices from './app/modules/users/users.services'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Application router
+app.use('/api/v1/users', usersRouter)
 
 // Testing
 app.get('/', async (req: Request, res: Response) => {
