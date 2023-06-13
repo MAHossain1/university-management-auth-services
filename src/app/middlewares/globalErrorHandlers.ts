ab026a8a74df1bd6a921fd4919f60a2dafdfac1c
MAHossain1 <mahpro110@gmail.com>
2023-06-13 10:20:55 +0600

    academic faculty done

--- src/app/middlewares/globalErrorHandlers.ts
+++ src/app/middlewares/globalErrorHandlers.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-expressions */
-import { ErrorRequestHandler } from 'express';
+import { ErrorRequestHandler, NextFunction } from 'express';
 import { Error } from 'mongoose';
 import config from '../../config';
 import { IGenericErrorMessage } from '../../interfaces/error';
@@ -10,7 +10,12 @@ import handleZodError from '../../errors/handleZodError';
 import { ZodError } from 'zod';
 import handleCastError from '../../errors/handleCastError';
 
-const globalErrorHandler: ErrorRequestHandler = (error, req, res) => {
+const globalErrorHandler: ErrorRequestHandler = (
+  error,
+  req,
+  res,
+  next: NextFunction
+) => {
   config.env === 'development'
     ? // eslint-disable-next-line no-console
       console.log('🚀globalErrorHandler ~~', error)
@@ -64,6 +69,7 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res) => {
     errorMessages,
     stack: config.env !== 'production' ? error?.stack : undefined,
   });
+  next();
 };
 
 export default globalErrorHandler;
